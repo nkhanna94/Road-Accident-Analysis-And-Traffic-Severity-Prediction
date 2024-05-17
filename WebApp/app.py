5ff93b53b2011753c328e9f609eb7b9cbf31a8e7
Niharika Khanna <118755402+nkhanna94@users.noreply.github.com>
2024-05-17 22:55:16 +0530

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,21 +1,56 @@
 from flask import Flask, render_template, request
-import random  # Import the random module for generating random numbers
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+import joblib
+import traceback
+import pickle
+
+model = pickle.load(open(r'D:\Python\severity_prediction\xgboost_modelv3.pkl', 'rb'))
 
 app = Flask(__name__)
-
-# Render the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Handle the form submission
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Generate a random number between 1 and 4
-    random_number = random.randint(1, 4)
+    distance = request.form['distance']
+    street = request.form['street']
+    city = request.form['city']
+    country = request.form['country']
+    state = request.form['state']
+    temperature = request.form['temperature']
+    wind_chill = request.form['wind_chill']
+    visibility = request.form['visibility']
+    wind_direction = request.form['wind_direction']
+    weather_condition = request.form['weather_condition']
+    traffic_signal = request.form['traffic_signal']
+    sunrise_sunset = request.form['sunrise_sunset']
 
-    # Render the result page with the random number
-    return render_template('result.html', random_number=random_number)
+    # Create a DataFrame with the input values
+    input_data = pd.DataFrame({
+        'distance': [distance],
+        'street': [street],
+        'city': [city],
+        'country': [country],
+        'state': [state],
+        'temperature': [temperature],
+        'wind_chill': [wind_chill],
+        'visibility': [visibility],
+        'wind_direction': [wind_direction],
+        'weather_condition': [weather_condition],
+        'traffic_signal': [traffic_signal],
+        'sunrise_sunset': [sunrise_sunset]
+    })
 
+    # Use the trained model to make a prediction
+    prediction = model.predict(input_data)[0]
+
+    # Render the result page with the prediction
+    return render_template('result.html', prediction=prediction)
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
+
